2bf1fd94132e65e5fb79dbbb8d0ef3faf4c5bb50
Bat-Ulzii Orgil <48145722+JohnJorjill@users.noreply.github.com>
2020-05-27 17:19:06 +0800

    added ListView

--- app/src/main/java/com/example/memorable_places/MainActivity.java
+++ app/src/main/java/com/example/memorable_places/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.memorable_places;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ListView listView = findViewById(R.id.listView); //connects listview
+
+        ArrayList<String> places = new ArrayList<String>(); // create a new arraylist
+
+        places.add("Add a new place..."); // adding a string into places arraylist
+
+        // creating a new arrayAdapter and adding places arraylisti into this
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, places);
         
     }
 }
